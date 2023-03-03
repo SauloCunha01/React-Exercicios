8d233bf444fea511dad4aceb050ce3c593e4da4d
SauloCunha <saulocunha01@hotmail.com>
2023-03-03 13:36:29 -0300

    c1
    s

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BoasVindas from './Components/BoasVindas'
+function App() {
+  return (
+    <div className="App" >
+      <BoasVindas nome="Saulo Cunha de Sousa"/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BoasVindas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+/* 
+1-	Crie um componente que renderize uma mensagem de boas-vindas. O componente deve receber o nome do usuário como propriedade e exibir a mensagem "Bem-vindo, {nome do usuário}!".
+*/
+export default (props)=>{
+    const nome = props.nome
+    return(
+        <div>
+            <h1>Bem-vindo , {nome}</h1>
+            <h5>Esta é uma pagina dedicada aos exercícios de REACT</h5>
+        </div>
+    )
+}
